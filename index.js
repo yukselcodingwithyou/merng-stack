d2816bb1ad6ab08241c109c6cce8b3e5c954d4c4
yukselcodingwithyou <yuksel.ozdemir@dolap.com>
2021-05-13 16:05:42 +0300

    03. rearrange file structure

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 
-const Post = require('./models/Post')
-const User = require('./models/User')
 const { MONGO_CONNECTION_STRING } = require('./config.js')
 
-const typeDefs = gql `
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-
-    type Query {
-        sayHello: String!
-        getPosts: [Post]
-    }
-`
-
-const resolvers = {
-    Query: {
-        sayHello: () => "Hello, World!",
-        async getPosts() {
-            try {
-                const posts = await Post.find();
-                return posts;
-            } catch (err) {
-                throw new Error(err);
-            }
-        }
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
